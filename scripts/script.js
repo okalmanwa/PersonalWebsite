8fdeebd5ea66163228d435b16d677496d5691671
Carey Manwa <okal@dhcp-vl2042-1364.redrover.cornell.edu>
2023-10-16 20:44:31 -0400

    Changed the animation to fade

--- scripts/script.js
+++ scripts/script.js
@@ -8,118 +8,101 @@ window.addEventListener('scroll', () => {
     }
 });
 
-
 const animationDuration = 1000;
-const animationDuration2= 2000;
+const animationDuration2 = 2000;
 
 // SCHOOL JS
-$('.school').click(function(){
-    $('#description').removeClass('hidden').slideDown(animationDuration2);
+$('.school').click(function () {
+    $('#description').removeClass('hidden').fadeIn(animationDuration2);
     $('.school').addClass('hidden');
 
-    $('.HotelManagement').slideUp(animationDuration);
-    $('.Tech').slideUp(animationDuration);
+    $('.HotelManagement').fadeOut(animationDuration);
+    $('.Tech').fadeOut(animationDuration);
 });
 
-$('#close1').click(function(){
-    $('#description').slideUp(animationDuration);
-    $('.school').removeClass('hidden');
+$('#close1').click(function () {
+    $('#description').fadeOut(animationDuration, function () {
+        $('#description').addClass('hidden');
+        $('.school').removeClass('hidden');
+    });
 
-    $('.HotelManagement').slideDown(animationDuration);
-    $('.Tech').slideDown(animationDuration);
+    $('.HotelManagement').fadeIn(animationDuration);
+    $('.Tech').fadeIn(animationDuration);
 });
 
 // PROJECT JS
-$('.HotelManagement').click(function(){
-    $('#project').removeClass('hidden').slideDown(animationDuration2);
+$('.HotelManagement').click(function () {
+    $('#project').removeClass('hidden').fadeIn(animationDuration2);
     $('.HotelManagement').addClass('hidden');
 
-    $('.school').slideUp(animationDuration);
-    $('.Tech').slideUp(animationDuration);
+    $('.school').fadeOut(animationDuration);
+    $('.Tech').fadeOut(animationDuration);
 });
 
+$('#close2').click(function () {
+    $('#project').fadeOut(animationDuration, function () {
+        $('#project').addClass('hidden');
+        $('.HotelManagement').removeClass('hidden');
+    });
 
-
-$('#close2').click(function(){
-    $('.HotelManagement').removeClass('hidden');
-    $('#project').slideUp(animationDuration);
-
-    $('.school').slideDown(animationDuration);
-    $('.Tech').slideDown(animationDuration);
+    $('.school').fadeIn(animationDuration);
+    $('.Tech').fadeIn(animationDuration);
 });
 
 // STUDENT TRAINING JS
-$('.Tech').click(function(){
-    $('#student_training').removeClass('hidden').slideDown(animationDuration2);
+$('.Tech').click(function () {
+    $('#student_training').removeClass('hidden').fadeIn(animationDuration2);
     $('.Tech').addClass('hidden');
 
-    $('.HotelManagement').slideUp(animationDuration);
-    $('.school').slideUp(animationDuration);
+    $('.HotelManagement').fadeOut(animationDuration);
+    $('.school').fadeOut(animationDuration);
 });
 
-$('#close3').click(function(){
-    $('.Tech').removeClass('hidden');
-    $('#student_training').slideUp(animationDuration);
+$('#close3').click(function () {
+    $('#student_training').fadeOut(animationDuration, function () {
+        $('#student_training').addClass('hidden');
+        $('.Tech').removeClass('hidden');
+    });
 
-    $('.HotelManagement').slideDown(animationDuration);
-    $('.school').slideDown(animationDuration);
+    $('.HotelManagement').fadeIn(animationDuration);
+    $('.school').fadeIn(animationDuration);
 });
 
+$(document).ready(function () {
+    var animationDuration = 'slow';
 
-
-
-$(document).ready(function() {
-    var animationDuration = 'slow'; 
-  
     $("#HamburgerBtn").click(function () {
-      if ($("#hamburgerMenuContainer").hasClass("hidden")) {
-        $("#hamburgerMenuContainer").removeClass("hidden").slideDown(animationDuration);
-      } else {
-        $("#hamburgerMenuContainer").addClass("hidden").slideUp(animationDuration);
-      }
+        if ($("#hamburgerMenuContainer").hasClass("hidden")) {
+            $("#hamburgerMenuContainer").removeClass("hidden").fadeIn(animationDuration);
+        } else {
+            $("#hamburgerMenuContainer").addClass("hidden").fadeOut(animationDuration);
+        }
     });
-  
-    $('a[href^="#"]').click(function(event) {
-      event.preventDefault();
-      var target = $(this.hash);
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000, function() {
-        $("#hamburgerMenuContainer").addClass("hidden").slideUp(animationDuration);
-      });
+
+    $('a[href^="#"]').click(function (event) {
+        event.preventDefault();
+        var target = $(this.hash);
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000, function () {
+            $("#hamburgerMenuContainer").addClass("hidden").fadeOut(animationDuration);
+        });
     });
-  });
+});
 
-
-
-
-  $(document).ready(function() {
+$(document).ready(function () {
     // Fade in each content element
-    $('.ExpPageContainer .contentHolder > div').each(function(index) {
+    $('.ExpPageContainer .contentHolder > div').each(function (index) {
         $(this).delay(index * 200).animate({ opacity: 1 }, 2000);
     });
 });
 
+$(document).ready(function () {
+    $('.nav-link').on('click', function () {
+        // Remove "active" class from all nav-links
+        $('.nav-link').removeClass('active');
 
-$(document).ready(function() {
-  $('.nav-link').on('click', function() {
-      // Remove "active" class from all nav-links
-      $('.nav-link').removeClass('active');
-      
-      // Add "active" class to the clicked nav-link
-      $(this).addClass('active');
-  });
+        // Add "active" class to the clicked nav-link
+        $(this).addClass('active');
+    });
 });
-
-
-
-
-
-  
-  
-
-
-  
-
-
- 
